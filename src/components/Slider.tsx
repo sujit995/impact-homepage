9c22eb7a6827f468b95ac352626e4c47b9d37a56
sujit995 <sujitmishra402@gmail.com>
2022-02-16 16:03:25 +0530

    minor changes in carousel

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,44 +1,9 @@
-// import 'bootstrap/dist/css/bootstrap.css';
-// import Carousel from 'react-bootstrap/Carousel';
-// import '../styles/slider.css';
 
-
-// export default function Slider() {
-// return (
-//           <div className="container-fluid" style={{backgroundColor:'#ddddee'}} id="carousel">
-//               <Carousel>
-//                 	<Carousel.Item>
-//                 			<div className="testimonial bg-color-2 text-center" id="carousel-items">
-// 								<img src="images/person_1.jpg" alt="Image" className="person_image" />
-// 								<h3 className="font-weight-bold">Maria Jones</h3>
-// 								<span className="position d-block mb-3">Customer</span>
-// 								<p>“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”</p>	
-// 							</div>
-//                 	</Carousel.Item>
-// 					<Carousel.Item>
-// 							<div className="testimonial bg-color-2 text-center" id="carousel-items">
-// 								<img src="images/person_2.jpg" alt="Image" className="person_image"/>
-// 								<h3 className="font-weight-bold">Maria Jones</h3>
-// 								<span className="position d-block mb-3">Customer</span>
-// 								<p>“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”</p>
-// 							</div>
-// 					</Carousel.Item>
-//                 	<Carousel.Item>
-//                 			<div className="testimonial bg-color-2 text-center" id="carousel-items">
-// 								<img src="images/person_3.jpg" alt="Image" className="person_image"/>
-// 								<h3 className="font-weight-bold">Maria Jones</h3>
-// 								<span className="position d-block mb-3">Customer</span>
-// 								<p>“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”</p>
-// 							</div>
-//                 	</Carousel.Item>
-//               </Carousel>
-//           </div>
-// );
-// }
 
 
 import React, {useState} from 'react'
 import { Carousel } from 'react-bootstrap';
+import '../styles/slider.css';
 
 type Props = {}
 
@@ -88,5 +53,8 @@ const Slider = (props: Props) => {
 }
 
 export default Slider
+
+
+
 
 
